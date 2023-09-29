ee307a00fe31ce0158686025d9d0010fc60ed83a
yeafla530 <yeafla530@naver.com>
2023-09-30 00:36:29 +0900

    [+] 0930 list 컴포넌트화

---
+++ typescript_with_react/02-vite-react-ts/src/components/TodoItems.tsx
@@ -0,0 +1,5 @@
+function TodoItems(props: {text: string}) {
+    return <li> {props.text}</li>
+}
+
+export default TodoItems
--- typescript_with_react/02-vite-react-ts/src/components/Todos.tsx
+++ typescript_with_react/02-vite-react-ts/src/components/Todos.tsx
@@ -1,11 +1,12 @@
 import {Todo} from '../models/todo'
+import TodoItems from './TodoItems'
 
 // 제너릭 사용
 function Todos(props: {items: Todo[]}) {
     return (
         <ul>
             {props.items.map((item) => (
-                <li key={item.id}>{item.text}</li>
+                <TodoItems key={item.id} text={item.text}/>
             ))}
         </ul>
     ) 
